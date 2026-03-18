c54c5742ebe0841156cd9d7831068c7d908855c8
kluczi <bartekcsgo04@gmail.com>
2026-03-18 12:40:05 +0100

    Add select helpers to fetch all rows from raw tables

---
+++ src/db/select.py
@@ -0,0 +1,37 @@
+import psycopg
+
+
+def fetch_all_from_projects(conn: psycopg) -> list:
+    results = []
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT * FROM raw.raw_projects;
+            """
+        )
+        results = cur.fetchall()
+    return results
+
+
+def fetch_all_from_ust(conn: psycopg):
+    results = []
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT * FROM raw.raw_user_subscription_tracking;
+            """
+        )
+        results = cur.fetchall()
+    return results
+
+
+def fetch_all_from_signups(conn: psycopg):
+    results = []
+    with conn.cursor() as cur:
+        cur.execute(
+            """
+            SELECT * FROM raw.signups;
+            """
+        )
+        results = cur.fetchall()
+    return results
